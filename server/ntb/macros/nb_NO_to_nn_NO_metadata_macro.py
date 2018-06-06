2e2596fdc801240d9ac767a8da64ba8c3c800f2f
jo-christian-oterhals <39984908+jo-christian-oterhals@users.noreply.github.com>
2018-06-06 10:04:09 +0200

    Translation added nb_NO_to_nn_NO_metadata_macro.py
    Added a call to a nb to nn translation service.

--- server/ntb/macros/nb_NO_to_nn_NO_metadata_macro.py
+++ server/ntb/macros/nb_NO_to_nn_NO_metadata_macro.py
@@ -5,6 +5,8 @@
     - change the service (ANPA category) to "NPKTema"
 """
 
+import requests
+from requests.auth import HTTPBasicAuth
 
 def nb_NO_to_nn_NO_metadata_macro(item, **kwargs):
     item['body_footer'] = '(©NPK)'
@@ -18,13 +20,23 @@ def nb_NO_to_nn_NO_metadata_macro(item, **kwargs):
             'scheme': None
         }
     ]
+    """
+        Translation start
+    """
+    payload = {k: item.get(k) for k in item if k in ('guid', 'evolvedfrom', 'versioncreated', 'headline', 'description_text', 'description_html', 'body_text', 'body_html')}
+    r = requests.post('http://api.smalldata.no:8080/translate', json=payload, auth=HTTPBasicAuth('superdesk', 'babel'))
+    item.update(r.json())
+    """
+        Translation end
+    """
     return item
 
 
 name = 'Bokmal to Nynorsk Metadata Macro'
 label = 'Translate to Nynorsk Macro'
 callback = nb_NO_to_nn_NO_metadata_macro
-access_type = 'backend'
+# access_type = 'backend'
+access_type = 'frontend' # For test purposes
 action_type = 'direct'
 from_languages = ['nb-NO']
 to_languages = ['nn-NO']
