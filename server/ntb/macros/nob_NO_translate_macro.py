bcf2c6ffb13734d975631dd5208ebf16c2c1ee53
jo-christian-oterhals <39984908+jo-christian-oterhals@users.noreply.github.com>
2018-06-19 11:49:59 +0200

    add macros for item translation (#237)
    + Macro for storing published/corrected article
    
    This macro is meant for use with articles that have been auto translated to nynorsk from bokmål. In most cases reporters will make corrections since the auto translation is not perfect. This macro saves the corrected/published version in the translator's database. (Will be used in the future for a kind of correction middleware)

---
+++ server/ntb/macros/nob_NO_translate_macro.py
@@ -0,0 +1,24 @@
+"""
+    nb-NO to nn-NO Metadata Macro will perform the following changes to current content item:
+    - change the language to nn-NO
+"""
+
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+def nob_NO_translate_macro(item, **kwargs):
+    creds = HTTPBasicAuth('superdesk', 'babel')
+    r = requests.post('http://api.smalldata.no:8080/translate', data=item, timeout=(10, 30), auth=creds)
+    if r.status_code == 200:
+        item.update(r.json())
+    return item
+
+
+name = 'Bokmal to Nynorsk Translate Macro'
+label = 'Omsett NB til NN'
+callback = nob_NO_translate_macro
+access_type = 'frontend'
+action_type = 'direct'
+from_languages = ['nb-NO']
+to_languages = ['nn-NO']
