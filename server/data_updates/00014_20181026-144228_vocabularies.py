0bfc66ac47578971b7f875878d3356c62bd5f4e1
Tomas Kikutis <t.kikutis@gmail.com>
2018-10-26 15:03:45 +0300

    add schema_field so category is available in planning

---
+++ server/data_updates/00014_20181026-144228_vocabularies.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8; -*-
+# This file is part of Superdesk.
+# For the full copyright and license information, please see the
+# AUTHORS and LICENSE files distributed with this source code, or
+# at https://www.sourcefabric.org/superdesk/license
+#
+# Author  : tomas
+# Creation: 2018-10-26 14:42
+
+from superdesk.commands.data_updates import DataUpdate
+
+
+class DataUpdate(DataUpdate):
+
+    resource = 'vocabularies'
+
+    def forwards(self, mongodb_collection, mongodb_database):
+        print(mongodb_collection.update(
+            {'_id': 'category'},
+            {'$set': {
+                'schema_field': 'subject'
+            }}
+        ))
+
+    def backwards(self, mongodb_collection, mongodb_database):
+        print(mongodb_collection.update(
+            {'_id': 'category'},
+            {'$unset': {
+                'schema_field': ''
+            }}
+        ))
