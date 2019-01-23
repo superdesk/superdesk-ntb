fa6c5da9dc5f526504347d58144ce295465b16b9
Petr Jasek <jasekpetr@gmail.com>
2019-01-23 16:06:19 +0100

    fix wrong schema field in vocabulary using migrations
    it must be unset on ntb-next, empty string doesn't work really
    
    SDNTB-550

--- server/ntb/publish/ntb_nitf.py
+++ server/ntb/publish/ntb_nitf.py
@@ -33,7 +33,7 @@ FILENAME_FORBIDDEN_RE = re.compile(r"[^a-zA-Z0-9._-]")
 STRIP_INVALID_CHARS_RE = re.compile('[\x00-\x08\x0B-\x0C\x0E-\x1F\x7F]')
 ENCODING = 'iso-8859-1'
 LANGUAGE = 'nb-NO'  # default language for ntb
-assert ENCODING is not 'unicode'  # use e.g. utf-8 for unicode
+assert ENCODING != 'unicode'  # use e.g. utf-8 for unicode
 
 
 def _get_rewrite_sequence(article):
