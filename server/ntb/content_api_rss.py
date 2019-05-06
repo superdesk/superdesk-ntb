ead6d438d22476e50a73af52cc656489475ad4a9
Petr Jasek <jasekpetr@gmail.com>
2019-05-06 16:25:31 +0200

    initial feed

--- server/ntb/content_api_rss.py
+++ server/ntb/content_api_rss.py
@@ -4,9 +4,26 @@ import superdesk
 from werkzeug.datastructures import MultiDict
 from flask import current_app as app, request
 from eve.utils import ParsedRequest
+from feedgen import feed
 
 
 blueprint = flask.Blueprint('rss', __name__)
+
+
+def generate_rss(items):
+    fg = feed.FeedGenerator()
+    fg.title('Newshub feed')
+    fg.link(href=flask.url_for('rss.index', _external=True), rel='self')
+    fg.description('foo')
+    for item in items:
+        entry = fg.add_entry()
+        entry.id(item['_id'])
+        entry.title(item.get('headline', item.get('name', item.get('slugline', ''))))
+        entry.description(item.get('description_text'))
+        entry.published(item['_created'])
+        entry.updated(item['versioncreated'])
+        entry.content(item.get('body_html'), type='text/html')
+    return fg.rss_str(pretty=True)
 
 
 @blueprint.route('/')
@@ -20,7 +37,9 @@ def index():
     items = []
     for item in res:
         items.append(item)
-    return flask.jsonify(items)
+
+    content = generate_rss(items)
+    return flask.Response(content, mimetype='application/rss+xml')
 
 
 def init_app(_app):
