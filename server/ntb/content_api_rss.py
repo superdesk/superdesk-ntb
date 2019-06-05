dec287b39e346b64725ee3ec684cd8c37b72e7e8
Petr Jašek <jasekpetr@gmail.com>
2019-06-05 13:45:04 +0200

    add msn feed output (#317)
    SDNTB-578

---
+++ server/ntb/content_api_rss.py
@@ -0,0 +1,80 @@
+import re
+import flask
+import superdesk
+
+from lxml import etree
+from feedgen import feed
+from flask import current_app as app, request
+from eve.utils import ParsedRequest
+
+
+blueprint = flask.Blueprint('rss', __name__)
+parser = etree.HTMLParser(recover=True)
+
+
+def generate_feed(items):
+    fg = feed.FeedGenerator()
+    fg.title('Superdesk')
+    fg.id(flask.url_for('rss.index', _external=True))
+    fg.link(href=flask.url_for('rss.index', _external=True), rel='self')
+    fg.description('foo')
+    for item in items:
+        entry = fg.add_entry()
+        entry.guid('{}/{}'.format(flask.url_for('rss.index', _external=True).rstrip('/'), item['_id']))
+        entry.title(item.get('headline', item.get('name', item.get('slugline', ''))))
+        entry.pubDate(item.get('firstpublished'))
+        entry.updated(item['versioncreated'])
+        entry.content(get_content(item), type='CDATA')
+
+        category = [{'term': s.get('name')} for s in item.get('subject', []) if s.get('scheme') == 'category']
+        if category:
+            entry.category(category)
+
+        if item.get('description_text'):
+            entry.summary(item['description_text'])
+
+        if item.get('byline'):
+            entry.author({'name': item['byline']})
+
+    return fg.atom_str(pretty=True) \
+        .replace(b'<content type="CDATA">', b'<content type="html">')
+
+
+def get_content(item):
+    assoc = item.get('associations') or {}
+    html = item.get('body_html', '<p></p>').replace('<figcatpion>', '<figcaption>')
+    tree = etree.fromstring(html, parser=parser)
+    body = tree[0]
+    for elem in body:
+        if elem.tag is etree.Comment and elem.text:
+            m = re.fullmatch(r'EMBED START Image {id: "([-_a-z0-9]+)"}', elem.text.strip())
+            if m:
+                _id = m.group(1)
+                related = assoc.get(_id)
+                if related and related.get('renditions') and related['renditions'].get('original'):
+                    img = elem.getnext().find('img')
+                    img.attrib['src'] = related['renditions']['original']['href']
+                    img.attrib['alt'] = related.get('description_text')
+    return etree.tostring(body, method='html') \
+        .decode('utf-8') \
+        .replace('<body>', '') \
+        .replace('</body>', '')
+
+
+@blueprint.route('/rss')
+def index():
+    auth = app.auth
+    if not auth.authorized([], 'items', request.method):
+        return auth.authenticate()
+    req = ParsedRequest()
+    req.args = request.args
+    res = superdesk.get_resource_service('items').get(req, {})
+    items = []
+    for item in res:
+        items.append(item)
+    content = generate_feed(items)
+    return flask.Response(content, mimetype='application/atom+xml')
+
+
+def init_app(_app):
+    _app.register_blueprint(blueprint, url_prefix='/contentapi')
---
+++ server/ntb/tests/msn_test.py
@@ -0,0 +1,40 @@
+
+import os
+import json
+import flask
+import unittest
+import feedparser
+
+from ntb.content_api_rss import get_content, generate_feed, init_app
+
+
+with open(os.path.join(os.path.dirname(__file__), 'fixtures', 'contentapi_item.json')) as _file:
+    ITEM = json.load(_file)
+
+
+class MSNFeedTestCase(unittest.TestCase):
+
+    def test_get_content(self):
+        content = get_content(ITEM)
+        self.assertEqual('''<p>with image</p>
+<p>foo</p>
+<!-- EMBED START Image {id: "editor_0"} -->
+<figure>
+    <img src="https://example.com/original.jpg" alt="description">
+    <figcaption>caption</figcaption>
+</figure>
+<!-- EMBED END Image {id: "editor_0"} -->''', content)
+
+    def test_atom(self):
+        app = flask.Flask(__name__)
+        app.config['SERVER_NAME'] = 'localhost'
+        init_app(app)
+        with app.test_request_context('/'):
+            xml = generate_feed([ITEM])
+
+        feed = feedparser.parse(xml)
+        self.assertEqual('http://localhost/contentapi/rss', feed.feed.id)
+
+        entry = feed.entries[0]
+        self.assertEqual('%s/%s' % (feed.feed.id, ITEM['_id']), entry.id)
+        self.assertEqual(ITEM['subject'][0]['name'], entry.category)
