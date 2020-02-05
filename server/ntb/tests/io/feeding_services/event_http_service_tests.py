47146a7c2c6c3cfc83229769e6b1ee68bfa21daa
Oleg Pshenichniy <olegpshenichniy@gmail.com>
2020-02-05 13:50:31 +0100

    Fix EventHTTPFeedingService test. Update planning. (#367)
    * SDNTB-622 [INGEST] Fix EventHTTPFeedingService test.
    
    * Update planning.

--- server/ntb/tests/io/feeding_services/event_http_service_tests.py
+++ server/ntb/tests/io/feeding_services/event_http_service_tests.py
@@ -1,6 +1,7 @@
 import os
 from unittest import mock
 
+from superdesk.io.feeding_services import http_base_service
 from planning.feeding_services import event_http_service
 from planning.tests import TestCase
 
@@ -17,21 +18,26 @@ class EventHTTPFeedingServiceTestCase(TestCase):
     def setUp(self):
         super().setUp()
 
-    @mock.patch.object(event_http_service, 'requests')
+    @mock.patch.object(http_base_service, 'requests')
     def test_update_ntb(self, requests):
         dirname = os.path.dirname(os.path.realpath(__file__))
         fixture = os.path.normpath(os.path.join(dirname, '../fixtures', 'ntb_event.xml'))
 
         with open(fixture, 'rb') as f:
-            requests.get.return_value = MockResponse(status_code=200, content=f.read())
+            response = MockResponse(status_code=200, content=f.read())
+            response.ok = True
+            requests.get.return_value = response
 
         service = event_http_service.EventHTTPFeedingService()
         provider = {
             'feed_parser': 'ntb_event_xml',
             'config': {
                 'url': 'https://example.com/NTBEvent.xml',
-            }
+            },
+            'is_closed': False,
+            '_id': 'ntb_event_xml',
+            'name': 'ntb_event_xml',
         }
 
-        events = list(service._update(provider, None))
+        events = list(service.update(provider, {}))
         self.assertEqual(len(events), 1)
