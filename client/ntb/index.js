c716139aa4dd8ebfbbdc50a3e31dcb107ee4f0c4
Petr <petr.jasek@sourcefabric.org>
2020-05-20 16:18:48 +0200

    set ntb module dependency
    to force run order

--- client/ntb/index.js
+++ client/ntb/index.js
@@ -1,8 +1,9 @@
 import angular from 'angular';
 
-export default angular.module('ntb', [])
+export default angular.module('ntb', ['superdesk.core.auth.login'])
     .run(['$templateCache', ($templateCache) => {
         // replace core login template with custom
+        console.info('ntb');
         $templateCache.put(
             'scripts/core/auth/login-modal.html',
             require('./views/login-modal.html'),
