3b83cf7987574e0cac76ebeaf4c5c0039e30d535
Tanuj Soni <tanuj.soni@sourcefabric.org>
2020-07-08 15:27:21 +0530

    SDNTB-638 Fixed flake8 errors

--- server/ntb/io/feed_parsers/ntb_tt_ninjs.py
+++ server/ntb/io/feed_parsers/ntb_tt_ninjs.py
@@ -7,38 +7,37 @@
 # For the full copyright and license information, please see the
 # AUTHORS and LICENSE files distributed with this source code, or
 # at https://www.sourcefabric.org/superdesk/license
-​
+
 from superdesk.io.feed_parsers.ninjs import NINJSFeedParser
 from superdesk.io.registry import register_feed_parser
 from superdesk.utc import utc
 from dateutil.parser import parse
 import json
-​
-​
+
+
 class NTBTTNINJSFeedParser(NINJSFeedParser):
     """
     Feed Parser for NTB TT NINJS format
     """
-​
+
     NAME = 'ntb_tt_ninjs'
-​
+
     label = 'NTB TT NINJS Feed Parser'
-​
+
     def __init__(self):
         super().__init__()
-​
+
     def parse(self, file_path, provider=None):
         self.items = []
         with open(file_path, 'r') as f:
             ninjs = json.load(f)
-​
+
         self.items.append(self._transform_from_ninjs(ninjs))
         self.is_sport_item = ninjs.get('sector') == 'SPT'
         return list(map(
             self._transform_to_ntb_tt_ninjs, self.items
         ))
-​
-​
+
     def _transform_to_ntb_tt_ninjs(self, item):
         subject_name = 'Sport' if self.is_sport_item else 'Utenriks'
         item['subject'] = item.get('subject', []) + [{
@@ -46,16 +45,16 @@ class NTBTTNINJSFeedParser(NINJSFeedParser):
             'scheme': 'category',
             'qcode': subject_name
         }]
-​
+
         item['anpa_category'] = item.get('anpa_category', []) + [{
             'name': 'Nyhetstjenesten',
             'qcode': 'n'
         }]
-​
+
         return item
-​
+
     def datetime(self, string):
         return parse(string).replace(tzinfo=utc)
-​
-​
+
+
 register_feed_parser(NTBTTNINJSFeedParser.NAME, NTBTTNINJSFeedParser())
