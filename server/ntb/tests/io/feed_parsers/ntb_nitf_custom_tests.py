4df2d33a8e7f1caed882c60825af03085813c73f
GyanP <gyanig72@gmail.com>
2021-06-22 22:09:27 +0530

    NTB custom NITF parser to store place and dateline [SDNTB-693] (#414)

---
+++ server/ntb/tests/io/feed_parsers/ntb_nitf_custom_tests.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8; -*-
+#
+# This file is part of Superdesk.
+#
+# Copyright 2013, 2014 Sourcefabric z.u. and contributors.
+#
+# For the full copyright and license information, please see the
+# AUTHORS and LICENSE files distributed with this source code, or
+# at https://www.sourcefabric.org/superdesk/license
+
+from ntb.io.feed_parsers.ntb_nitf_custom import NTBNITFCustomFeedParser
+from . import XMLParserTestCase
+
+
+class NTBNITFCustomTestCase(XMLParserTestCase):
+
+    filename = 'nitf_test.xml'
+    parser = NTBNITFCustomFeedParser()
+
+    def test_dateline(self):
+        dateline = self.item.get("dateline")
+        self.assertEqual(dateline["located"]["city"], "Stockholm")
+
+    def test_place(self):
+        places = self.item.get("place", [])
+        self.assertEqual(2, len(places))
+        self.assertEqual("Norden", places[0]["name"])
+        self.assertEqual("Sverige", places[1]["name"])
