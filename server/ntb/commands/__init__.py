8d7219ffe5aa0bdb152b9458e2780aad1827b0e5
Mark Pittaway <mark.pittaway@gmail.com>
2021-08-09 15:25:02 +1000

    [SDNTB-701] Command to synchronise IPTC Media Topics

--- server/ntb/commands/__init__.py
+++ server/ntb/commands/__init__.py
@@ -2,6 +2,8 @@
 import superdesk
 
 from .update_items import UpdateItemsCommand
+from .sync_topics import SyncTopicsCommand
 
 
 superdesk.command('ntb:update_items', UpdateItemsCommand())
+superdesk.command('ntb:syn_topics', SyncTopicsCommand())
