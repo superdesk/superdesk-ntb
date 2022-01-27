e0fa16a56d9d7e2929d4895a86fe9337623e268a
Petr Jasek <jasekpetr@gmail.com>
2022-01-27 11:06:58 +0100

    fix test

--- server/ntb/tests/mediatopics_to_subject_mapping_test.py
+++ server/ntb/tests/mediatopics_to_subject_mapping_test.py
@@ -68,7 +68,7 @@ class MediatopicsToSubjectMappingTestCase(TestCase):
         populate_subject(None, item, foo="bar")
         self.assertEqual(6, len(item["subject"]))
 
-        self.assertEqual("10014000", item["subject"][4]["qcode"])
+        self.assertEqual("10004000", item["subject"][4]["qcode"])
         self.assertEqual(ntb.SUBJECTCODES_CV, item["subject"][4]["scheme"])
 
         self.assertEqual("10000000", item["subject"][5]["qcode"])
