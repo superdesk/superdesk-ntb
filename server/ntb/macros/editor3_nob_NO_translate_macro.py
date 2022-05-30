42c4f0c90c1346c3e9b1f9ae7f564dcd9d348410
Tanuj Soni <tanuj.soni@sourcefabric.org>
2022-05-30 13:49:22 +0530

    SDNTB-765 ADD: New nob-no-translate-macro specifically for editor3.

---
+++ server/ntb/macros/editor3_nob_NO_translate_macro.py
@@ -0,0 +1,23 @@
+"""
+nb-NO to nn-NO Metadata Macro will perform the following changes to current content item:
+- change the language to nn-NO
+"""
+
+from superdesk.editor_utils import generate_fields
+from .nob_NO_translate_macro import nob_NO_translate_macro
+
+
+def editor3_Nob_NO_translate_macro(item, **kwargs):
+    item = nob_NO_translate_macro(item, **kwargs)
+    breakpoint()
+    return item
+
+
+name = "Editor3 Bokmal to Nynorsk Translate Macro"
+label = "Editor3 Omsett NB til NN"
+callback = editor3_Nob_NO_translate_macro
+access_type = "frontend"
+action_type = "direct"
+from_languages = ["nb-NO"]
+to_languages = ["nn-NO"]
+replace_type = "editor_state"
