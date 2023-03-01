d7ad7cb51d18afac2a27d705136b8dacc434fd47
Petr Jašek <jasekpetr@gmail.com>
2023-03-01 13:32:19 +0100

    fix missing fields in ninjs 2.0 (#446)
    SDNTB-801

--- server/ntb/tests/publish/ntb_ninjs_test.py
+++ server/ntb/tests/publish/ntb_ninjs_test.py
@@ -4,6 +4,7 @@ import pathlib
 from unittest import mock
 from ntb.publish.ntb_ninjs import NTBNINJSFormatter
 from superdesk.tests import TestCase
+from datetime import datetime
 
 
 FAMILY_ID = "abcd"
@@ -166,6 +167,39 @@ class Ninjs2FormatterTest(TestCase):
             "source": "NTB",
             "text": "HAMMERFEST, Sep 13  -",
         },
+        "genre": [
+            {"qcode": "genre code", "name": "genre name"},
+        ],
+        "anpa_category": [
+            {"name": "Omtaletjenesten", "qcode": "o", "language": "nb-NO"},
+        ],
+        "associations": {
+            "featuremedia": {
+                "_id": "test_id",
+                "guid": "test_id",
+                "headline": "feature headline",
+                "ingest_provider": "fdsfdsfsdfs",
+                "original_source": "feature_source",
+                "pubstatus": "usable",
+                "renditions": {
+                    "original": {
+                        "href": "http://scanpix.no/spWebApp/previewimage/sdl/preview_big/test_id.jpg",
+                        "mimetype": "image/jpeg",
+                    },
+                    "thumbnail": {
+                        "href": "http://preview.scanpix.no/thumbs/tb/4/33/test_id.jpg"
+                    },
+                    "viewImage": {
+                        "href": "http://scanpix.no/spWebApp/previewimage/sdl/preview/test_id.jpg"
+                    },
+                },
+                "source": "feature_source",
+                "fetch_endpoint": "scanpix",
+                "type": "picture",
+                "versioncreated": datetime(2023, 3, 1, 12, 10, 0),
+                "description_text": "test feature media",
+            },
+        },
     }
 
     def setUp(self):
@@ -182,6 +216,7 @@ class Ninjs2FormatterTest(TestCase):
                     self.app.data.insert("vocabularies", [cv])
         seq, doc = self.formatter.format(self.article, {"name": "Test Subscriber"})[0]
         ninjs = json.loads(doc)
+        assoc = ninjs.pop("associations")
         expected_item = {
             "guid": "123",
             "version": "1",
@@ -256,7 +291,8 @@ class Ninjs2FormatterTest(TestCase):
                     "name": "Gjerdrum",
                     "uri": "http://www.wikidata.org/entity/Q57084",
                     "literal": "b564b1e1-1a99-324e-b643-88e5398305c6",
-                    "countydist": "Gjerdrum",
+                    "county-dist": "Gjerdrum",
+                    "state-prov": "Viken",
                 },
                 {
                     "name": "Global",
@@ -268,6 +304,38 @@ class Ninjs2FormatterTest(TestCase):
                 "foo@bar.com",
             ],
             "located": "Hammerfest",
+            "genre": [
+                {"code": "genre code", "name": "genre name"},
+            ],
+            "service": [
+                {"name": "Omtaletjenesten", "code": "o"},
+            ],
         }
 
         self.assertEqual(ninjs, expected_item)
+
+        self.assertEqual(assoc, [{
+            "name": "featuremedia",
+            "altids": [
+                {"role": "GUID", "value": "test_id"},
+            ],
+            "descriptions": [
+                {"contenttype": "text/plain", "value": "test feature media"},
+            ],
+            "guid": "test_id",
+            "headlines": [
+                {"contenttype": "text/plain", "value": "feature headline"},
+            ],
+            "renditions": [
+                {
+                    "name": "original",
+                    "href": "http://scanpix.no/spWebApp/previewimage/sdl/preview_big/test_id.jpg",
+                    "contenttype": "image/jpeg",
+                },
+            ],
+            "pubstatus": "usable",
+            "taglines": [],
+            "type": "picture",
+            "version": "1",
+            "versioncreated": "2023-03-01T12:10:00+0000",
+        }])
