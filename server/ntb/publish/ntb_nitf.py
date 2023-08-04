afd1f5914bb191698c68cb904e1a7d5f2e83f621
Ketan <73937490+devketanpro@users.noreply.github.com>
2023-08-04 12:55:36 +0530

    NTB NITF: Addition of id attribute to the evloc tag [SDNTB-817] (#461)
    * add new attribute id
    
    * update testcase
    
    * reformat code via black
    
    * fix flake8

--- server/ntb/publish/ntb_nitf.py
+++ server/ntb/publish/ntb_nitf.py
@@ -193,7 +193,9 @@ class NTBNITFFormatter(NITFFormatter):
 
     def _format_docdata_doc_id(self, article, docdata):
         etree.SubElement(
-            docdata, "doc-id", attrib={"regsrc": "NTB", "id-string": utils.get_doc_id(article)}
+            docdata,
+            "doc-id",
+            attrib={"regsrc": "NTB", "id-string": utils.get_doc_id(article)},
         )
 
     def _format_date_expire(self, article, docdata):
@@ -230,6 +232,7 @@ class NTBNITFFormatter(NITFFormatter):
         mapping = (
             ("state-prov", ("ntb_parent", "name")),
             ("county-dist", ("ntb_qcode", "qcode")),
+            ("id", ("wikidata", "altids")),
         )
         for place in article.get("place", []):
             if not place:
@@ -241,8 +244,13 @@ class NTBNITFFormatter(NITFFormatter):
             for attrib, keys in mapping:
                 for key in keys:
                     if data.get(key):
-                        evloc.attrib[attrib] = data[key]
-                        break
+                        if not data.get("wikidata") and key == "altids":
+                            evloc.attrib[attrib] = data.get("altids", {}).get(
+                                "wikidata"
+                            )
+                        else:
+                            evloc.attrib[attrib] = data[key]
+                            break
 
     def _format_pubdata(self, article, head):
         pub_date = article["versioncreated"].astimezone(tz).strftime("%Y%m%dT%H%M%S")
@@ -254,7 +262,6 @@ class NTBNITFFormatter(NITFFormatter):
         ] = pubdata  # needed to access pubdata when formatting body content
 
     def _format_subjects(self, article, tobject):
-
         # Call Function for mapping of Imatrics entities
         self._format_imatrics_entities(article, tobject)
 
@@ -395,7 +402,9 @@ class NTBNITFFormatter(NITFFormatter):
 
         # daily counter
         if app.config.get("NTB_IPTC_SEQUENCE"):
-            day_start = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+            day_start = datetime.now().replace(
+                hour=0, minute=0, second=0, microsecond=0
+            )
             pub_queue = get_resource_service("publish_queue")
             daily_count = (
                 pub_queue.find({"transmit_started_at": {"$gte": day_start}}).count() + 1
