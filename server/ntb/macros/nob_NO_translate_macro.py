ee41d72c9dc4dc5bc3a1679b3698d618f3ea550e
devketanpro <devketanpro11@gmail.com>
2024-10-17 15:27:05 +0530

    Bokmål to Nynorsk macro API update [SDNTB-875]

--- server/ntb/macros/nob_NO_translate_macro.py
+++ server/ntb/macros/nob_NO_translate_macro.py
@@ -4,20 +4,50 @@ nb-NO to nn-NO Metadata Macro will perform the following changes to current cont
 """
 
 import requests
-from requests.auth import HTTPBasicAuth
+from apps.auth import get_user
+from flask import current_app as app
 
 
 def nob_NO_translate_macro(item, **kwargs):
-    creds = HTTPBasicAuth("superdesk", "babel")
+    preference_params = {k: True for k in get_user_preference_params()}
+
+    token = app.config.get("OMSETT_API_TOKEN", "")
+
     payload = {
         k: item.get(k)
         for k in item
-        if k in ("guid", "headline", "body_html", "body_text", "abstract", "description_html", "description_text")
+        if k
+        in (
+            "guid",
+            "headline",
+            "body_html",
+            "body_text",
+            "abstract",
+            "description_html",
+            "description_text",
+            "evolvedfrom",
+        )
     }
-    r = requests.post("http://api.smalldata.no:8080/translate", data=payload, timeout=(10, 30), auth=creds)
+
+    data = {
+        "token": token,
+        "document": payload,
+        "prefs": preference_params,
+        "fileType": "html",
+    }
+
+    r = requests.post("https://nynorsk.cloud/translate", data=data, timeout=(10, 30))
+
     if r.status_code == 200:
         item.update(r.json())
     return item
+
+
+def get_user_preference_params():
+    user = get_user()
+    user_macro_preferences = user.get("user_preferences", {}).get("macro_config", {})
+    field_param = user_macro_preferences.get("fields").get("Formval nynorskrobot", "")
+    return field_param.split(",")
 
 
 name = "Bokmal to Nynorsk Translate Macro"
