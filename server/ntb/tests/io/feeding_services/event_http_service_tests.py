277bf3f0ebb5a448776c480525c1b633954f43c7
Petr Jasek <jasekpetr@gmail.com>
2024-10-22 14:59:44 +0200

    fix server CI

--- server/ntb/tests/io/feeding_services/event_http_service_tests.py
+++ server/ntb/tests/io/feeding_services/event_http_service_tests.py
@@ -1,42 +1,32 @@
 import os
-from unittest import mock
+import responses
 
-from superdesk.io.feeding_services import http_base_service
-from planning.feeding_services import event_http_service
 from planning.tests import TestCase
-
-
-class MockResponse:
-    def __init__(self, status_code, content):
-        self.status_code = status_code
-        self.content = content
-        self.headers = mock.MagicMock()
+from planning.feeding_services import event_http_service
 
 
 class EventHTTPFeedingServiceTestCase(TestCase):
-
-    def setUp(self):
-        super().setUp()
-
-    @mock.patch.object(http_base_service, 'requests')
-    def test_update_ntb(self, requests):
+    @responses.activate(assert_all_requests_are_fired=True)
+    def test_update_ntb(self):
         dirname = os.path.dirname(os.path.realpath(__file__))
-        fixture = os.path.normpath(os.path.join(dirname, '../fixtures', 'ntb_event.xml'))
+        fixture = os.path.normpath(
+            os.path.join(dirname, "../fixtures", "ntb_event.xml")
+        )
 
-        with open(fixture, 'rb') as f:
-            response = MockResponse(status_code=200, content=f.read())
-            response.ok = True
-            requests.get.return_value = response
+        with open(fixture, "rb") as f:
+            body = f.read()
+
+        responses.get("https://example.com/NTBEvent.xml", body=body, status=200)
 
         service = event_http_service.EventHTTPFeedingService()
         provider = {
-            'feed_parser': 'ntb_event_xml',
-            'config': {
-                'url': 'https://example.com/NTBEvent.xml',
+            "feed_parser": "ntb_event_xml",
+            "config": {
+                "url": "https://example.com/NTBEvent.xml",
             },
-            'is_closed': False,
-            '_id': 'ntb_event_xml',
-            'name': 'ntb_event_xml',
+            "is_closed": False,
+            "_id": "ntb_event_xml",
+            "name": "ntb_event_xml",
         }
 
         events = list(service.update(provider, {}))
