726a8e53ce0eeef622ab39d6fd19beb47e12b991
Ketan <73937490+devketanpro@users.noreply.github.com>
2024-10-22 10:05:51 +0530

    Bokmål to Nynorsk macro API update [SDNTB-875] (#497)
    * Bokmål to Nynorsk macro API update [SDNTB-875]
    
    * refromat code via black

--- server/ntb/__init__.py
+++ server/ntb/__init__.py
@@ -1,7 +1,16 @@
 import logging
+import superdesk
+from flask_babel import lazy_gettext
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 MEDIATOPICS_CV = "topics"
 SUBJECTCODES_CV = "subject_custom"
+
+superdesk.register_default_user_preference(
+    "macro_config",
+    {"fields": {"Formval nynorskrobot": ""}},
+    label=lazy_gettext("Macro config"),
+    category=lazy_gettext("Macro"),
+)
