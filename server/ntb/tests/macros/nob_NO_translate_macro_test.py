ed732088092057afd3cce133db34962c3b4d54ee
Ketan <73937490+devketanpro@users.noreply.github.com>
2025-01-03 20:59:52 +0530

    FIX : Strip white spaces in Bokmål to Nynorsk macro user preferences field [SDNTB-879] (#521)
    * fix : Strip white spaces in Bokmål to Nynorsk macro user preferences field [SDNTB-879]
    
    * update tests

--- server/ntb/tests/macros/nob_NO_translate_macro_test.py
+++ server/ntb/tests/macros/nob_NO_translate_macro_test.py
@@ -25,7 +25,9 @@ class TranslateMacroTestCase(TestCase):
     user_preferences = {
         "user_preferences": {
             "macro_config": {
-                "fields": {"Formval nynorskrobot": "headline,description_text"}
+                "fields": {
+                    "Formval nynorskrobot": " headline , description_text , , body_html , "
+                }
             }
         }
     }
@@ -65,3 +67,8 @@ class TranslateMacroTestCase(TestCase):
             item["associations"]["editor_1"]["description_text"],
             "Kva synest du om Noreg",
         )
+
+        # assert params SDNTB-879
+
+        params = nob_NO_translate_macro.get_user_preference_params()
+        self.assertEqual(params, ["headline", "description_text", "body_html"])
