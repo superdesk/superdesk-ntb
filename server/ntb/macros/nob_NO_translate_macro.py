c2b160aa53672f4495e49538785b75fb2d93c49e
devketanpro <devketanpro11@gmail.com>
2025-01-06 10:30:21 +0530

    Add support for translation exemption marks in Bokmål to Nynorsk macro
    Add [SDNTB-878]

--- server/ntb/macros/nob_NO_translate_macro.py
+++ server/ntb/macros/nob_NO_translate_macro.py
@@ -1,8 +1,4 @@
-"""
-nb-NO to nn-NO Metadata Macro will perform the following changes to current content item:
-- change the language to nn-NO
-"""
-
+import re
 import requests
 from apps.auth import get_user
 from flask import current_app as app
@@ -43,11 +39,17 @@ def nob_NO_translate_macro(item, **kwargs):
 
     payload.update(associations_desc_flat)
 
+    # exempt text within `«` and `»`
+    for field, value in payload.items():
+        if isinstance(value, str):
+            payload[field] = re.sub(r"«(.*?)»", r'<span lang="nb">\1</span>', value)
+
     data = {
         "token": token,
         "document": payload,
         "prefs": preference_params,
         "fileType": "html",
+        "lang_translate_parameter": None,
     }
 
     r = requests.post("https://nynorsk.cloud/translate", json=data, timeout=(10, 30))
@@ -56,13 +58,20 @@ def nob_NO_translate_macro(item, **kwargs):
         response = r.json()
         item.update(response["document"])
 
+        # restore exempted text by removing `<span lang="nb">` tags
+        for field, value in item.items():
+            if isinstance(value, str):
+                item[field] = re.sub(r"<span lang=\"nb\">(.*?)</span>", r"\1", value)
+
         # map translated `description_text` fields back to the associations.
         for editor in item.get("associations", {}):
             flat_key = f"associations_desc_{editor}"
             if flat_key in response["document"]:
-                item["associations"][editor]["description_text"] = response["document"][
-                    flat_key
-                ]
+                item["associations"][editor]["description_text"] = re.sub(
+                    r"<span lang=\"nb\">(.*?)</span>",
+                    r"\1",
+                    response["document"][flat_key],
+                )
     return item
 
 
